701c278ef331b391d0c596733ed80449d5ae3ee7
Лаисат <lz_256@mail.ru>
2023-11-15 19:20:24 +0300

    reference

--- src/Book.java
+++ src/Book.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Book {
     public final String name;
     public final Author author;
@@ -26,8 +28,25 @@ public class Book {
         return publicationYear;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Book book)) return false;
+        return publicationYear == book.publicationYear && Objects.equals(name, book.name) && Objects.equals(author, book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, author, publicationYear);
+    }
+
+    @Override
     public String toString() {
-        return author.toString();
+        return "Book{" +
+                "name='" + name + '\'' +
+                ", author=" + author +
+                ", publicationYear=" + publicationYear +
+                '}';
     }
 }
 
